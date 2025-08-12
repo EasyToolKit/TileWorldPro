389bc435456b2d385a7c11282363a68f097fc506
计都 <fuyouawa@gmail.com>
2025-08-12 21:34:29 +0800

    更新：完善瓦片构建处理器框架

--- Assets/Plugins/EasyToolKit/TileWorldPro/Runtime/Tools/Builder/Processors/Core/AbstractTileBuildProcessor.cs
+++ Assets/Plugins/EasyToolKit/TileWorldPro/Runtime/Tools/Builder/Processors/Core/AbstractTileBuildProcessor.cs
@@ -1,11 +1,13 @@
+using EasyToolKit.Inspector;
 using System;
 using UnityEngine;
 
 namespace EasyToolKit.TileWorldPro
 {
-    [Serializable]
+    [MetroFoldoutGroup("@Label")]
     public abstract class AbstractTileBuildProcessor : ITileBuildProcessor
     {
+        [LabelText("名称")]
         [SerializeField] private string _label;
         public string Label => _label;
 
